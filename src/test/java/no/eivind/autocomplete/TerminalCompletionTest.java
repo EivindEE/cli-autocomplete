a0ab7523ba506562b41dffd9e356a67b8ab8d69c
Eivind Eidheim Elseth <eivind.elseth@comperio.no>
2015-11-23 09:31:16 +0100

    Added tests for multiple matches

--- src/test/java/no/eivind/autocomplete/TerminalCompletionTest.java
+++ src/test/java/no/eivind/autocomplete/TerminalCompletionTest.java
@@ -1,6 +1,8 @@
 package no.eivind.autocomplete;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,6 +10,7 @@ import org.junit.Test;
 
 public class TerminalCompletionTest {
 	private TerminalCompletion completion;
+
 	@Before
 	public void setUp() throws Exception {
 		this.completion = new MapTerminalCompletion();
@@ -37,8 +40,40 @@ public class TerminalCompletionTest {
 	public void findPartialMatches() {
 		String command = "command";
 		completion.addToHistory(command);
-		String partialyMatchingCommand = command.substring(command.length() / 2) ;
+		String partialyMatchingCommand = command.substring(command.length() / 2);
 		assertTrue(completion.find(partialyMatchingCommand).contains(command));
 	}
 
+	@Test
+	public void findMultipelMatches() {
+		String partialMatch = "cd ";
+		String command1 = partialMatch + "/etc/";
+		String command2 = partialMatch + "~";
+		String command3 = partialMatch + "~/development";
+
+		String[] commands = { command1, command2, command3 };
+		for (String command : commands) {
+			completion.addToHistory(command);
+		}
+
+		assertEquals(commands.length, completion.find(partialMatch).size());
+	}
+	
+	@Test
+	public void findMultipelMatchesIgnoresNonMatching() {
+		String partialMatch = "cd ";
+		String command1 = partialMatch + "/etc/";
+		String command2 = partialMatch + "~";
+		String command3 = partialMatch + "~/development";
+
+		String[] commands = { command1, command2, command3 };
+		for (String command : commands) {
+			completion.addToHistory(command);
+		}
+		
+		// Add a non matching command
+		completion.addToHistory(partialMatch.substring(1));
+
+		assertEquals(commands.length, completion.find(partialMatch).size());
+	}
 }
